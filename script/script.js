81218afa86d6bc4d6cc18494c02e3858ad202be3
msameer10 <mohammadsameer0409@gmail.com>
2023-08-24 23:41:35 -0500

    push

--- script/script.js
+++ script/script.js
@@ -20,31 +20,27 @@ async function createArticleCards(query = '') {
 
   cardContainer.innerHTML = '';
 
-  // Shuffle the articles array randomly
-  const shuffledArticles = articles.sort(() => Math.random() - 0.5);
+  // Filter the articles based on the search query
+  const filteredArticles = articles.filter(article =>
+    article.title.toLowerCase().includes(query.toLowerCase())
+  );
 
-  // Filter and display the shuffled articles based on the search query
-  for (let i = 0; i < 4 && i < shuffledArticles.length; i++) {
-    const article = shuffledArticles[i];
-
-    // Check if the article title includes the search query (case-insensitive)
-    if (article.title.toLowerCase().includes(query.toLowerCase())) {
-      const card = document.createElement('div');
-      card.className = 'card mb-3';
-      card.style.marginRight = '10px'; // Add margin for horizontal gap between cards
-      card.innerHTML = `
-        <a href="${article.url}" target="_blank">
-          <img src="${article.imageUrl}" class="card-img-top" alt="${article.title}">
-          <div class="card-body">
-            <h5 class="card-title">${article.title}</h5>
-          </div>
-        </a>
-      `;
-      cardContainer.appendChild(card);
-    }
-  }
+  // Display the filtered articles
+  filteredArticles.slice(0, 4).forEach(article => {
+    const card = document.createElement('div');
+    card.className = 'card mb-3';
+    card.style.marginRight = '10px'; // Add margin for horizontal gap between cards
+    card.innerHTML = `
+      <a href="${article.url}" target="_blank">
+        <img src="${article.imageUrl}" class="card-img-top" alt="${article.title}">
+        <div class="card-body">
+          <h5 class="card-title">${article.title}</h5>
+        </div>
+      </a>
+    `;
+    cardContainer.appendChild(card);
+  });
 }
-
 
 // Create article cards on page load
 createArticleCards();
