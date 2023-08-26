f5fb034d3dc08e648b07d2856f97a0397aa3cd88
msameer10 <mohammadsameer0409@gmail.com>
2023-08-26 12:43:33 -0500

    push 1.0.3.12

--- script/article-script.js
+++ script/article-script.js
@@ -1,10 +1,10 @@
 const searchInput = document.getElementById('searchInput');
-const articleList = document.getElementById('articleList');
+const searchResultsContainer = document.getElementById('searchResultsContainer');
 
 // Function to fetch article data from data.json
 async function fetchArticleData() {
   try {
-    const response = await fetch('../data.json'); // Change the path if needed
+    const response = await fetch('../data.json');
     const data = await response.json();
     return data;
   } catch (error) {
@@ -15,39 +15,23 @@ async function fetchArticleData() {
 
 // Function to display search results as a dropdown
 function displaySearchResults(results) {
-  articleList.innerHTML = ''; // Clear existing results
+  searchResultsContainer.innerHTML = ''; // Clear existing results
 
   results.forEach(article => {
-    const option = document.createElement('option');
-    option.value = article.title;
-    articleList.appendChild(option);
+    const resultItem = document.createElement('option');
+    resultItem.value = article.title;
+    searchResultsContainer.appendChild(resultItem);
   });
 }
 
 // Search input event listener
-searchInput.addEventListener('input', async () => {
+searchInput.addEventListener('input', () => {
   const query = searchInput.value.toLowerCase();
 
-  const articles = await fetchArticleData();
-
-  const searchResults = articles.filter(article =>
-    article.title.toLowerCase().includes(query)
-  );
-
-  displaySearchResults(searchResults);
-});
-
-// Click event listener for suggestions
-articleList.addEventListener('input', async (event) => {
-  const selectedTitle = event.target.value;
-
-  const articles = await fetchArticleData();
-
-  const selectedArticle = articles.find(article =>
-    article.title.toLowerCase() === selectedTitle.toLowerCase()
-  );
-
-  if (selectedArticle) {
-    window.location.href = selectedArticle.url; // Navigate to the selected article
-  }
+  fetchArticleData().then(articles => {
+    const searchResults = articles.filter(article =>
+      article.title.toLowerCase().includes(query)
+    );
+    displaySearchResults(searchResults);
+  });
 });
